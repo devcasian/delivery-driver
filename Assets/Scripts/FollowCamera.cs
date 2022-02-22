376ca54d9f3bf05feff78e00e1110c97a77bf576
Dmitry Casian <webcasian@gmail.com>
2022-02-22 14:13:50 +0200

    Add simple follow camera

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    [SerializeField] private GameObject player;
+
+    private void LateUpdate()
+    {
+        transform.position = player.transform.position + new Vector3(0, 0, -10);
+    }
+}
